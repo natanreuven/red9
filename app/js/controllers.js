6cfc5a49d8f393f3288569cb0556b8c94eb74634
natan reuven <natanreuven@gmail.com>
2014-06-25 23:26:38 +0300

    get the number in

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,15 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
-
+	  $scope.board1  = [[1,3,6], [2,4,7] , [5,9,8] ] ;  
+	  
+  $scope.NumberIsClicked = function(obj) {
+	        console.log(obj) ; 
+	        /*alert(obj.target.attributes.data.value);*/
+	    }
+	  
+  
+  
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
