0b422ec76f9213c964d7a9a7ff810fbcbf9f2861
natan reuven <natanreuven@gmail.com>
2014-06-26 23:40:24 +0300

    logic has not ended

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,12 +4,50 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
-	  $scope.board1  = [[1,3,6], [2,4,7] , [5,9,8] ] ;  
+	  $scope.board1  = [[1,3,6], [2,4,7] , [5,9,8] ] ; 
+	  $scope.SortedArray = [] ; 
+	  $scope.IsFirstClick = 0; 
+	  $scope.NextElementIndex = 0 ; 
+	  for(var i = 0; i < $scope.board1.length; i++) {
+		    var row = $scope.board1[i];
+		    for(var j = 0; j < row.length; j++) {
+		    	$scope.SortedArray.push(row[j]);
+		        /*console.log("row[" + i + "][" + j + "] = " + row[j]);*/
+		       /* console.log($scope.SortedArray) ;*/ 
+		    }
+		  
+		}
+	  $scope.SortedArray.sort();
+	  $scope.FirstNum = $scope.SortedArray[0];
+	  $scope.lastElement = $scope.SortedArray[$scope.SortedArray.length - 1];
+	  console.log($scope.FirstNum) ; 
+	  console.log($scope.lastElement) ; 
+	  /*console.log($scope.SortedArray) ;*/
+	  
+	  
+	  
+	  
+	  
 	  
   $scope.NumberIsClicked = function(obj) {
-	        console.log(obj) ; 
-	        /*alert(obj.target.attributes.data.value);*/
-	    }
+	  if ($scope.NextElementIndex == $scope.SortedArray.length  )
+	     {   
+		  console.log ( " $scope.NextElementIndex " +  $scope.NextElementIndex  + 
+				  		" $scope.SortedArray.length " + $scope.SortedArray.length ) ; 
+		  console.log("you win ") ; 
+		 } 
+	  else  {  
+		   if (obj ==  $scope.SortedArray[$scope.NextElementIndex] )  
+		       {$scope.NextElementIndex = $scope.NextElementIndex + 1 ;
+			   console.log("  ok  ") ; }
+	       else {
+	    	   console.log(" wrong number  ") 
+	  			}
+	  		}
+		/*      console.log(obj) ; 
+	        console.log($scope.SortedArray) ;*/
+	        
+  }
 	  
   
   
